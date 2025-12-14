fd7f0200400eeddfed55bdbf4178b3659ab5cd80
Kami <a@b.com>
2025-12-13 17:54:00 -0800

    Orangutini Ananasini

---
+++ GER_flag_swap.bat
@@ -0,0 +1,12 @@
+@echo off
+cd .\gfx\map\shields
+ren shield_GER.bmp shield_GER_tmp.bmp
+ren shield_GER_2.bmp shield_GER.bmp
+ren shield_GER_tmp.bmp shield_GER_2.bmp
+cd ..\..\..\gfx\map\flags
+ren flag_GER.bmp flag_GER_tmp.bmp
+ren flag_GER_2.bmp flag_GER.bmp
+ren flag_GER_tmp.bmp flag_GER_2.bmp
+
+echo German Flag is Swapped
+pause
