9a16372a04157cda30f6ff17825e45a71216e734
Kami <a@b.com>
2025-12-13 19:57:15 -0800

    Burbaloni Lulilolli

--- GER_flag_swap.bat
+++ GER_flag_swap.bat
@@ -7,6 +7,10 @@ cd ..\..\..\gfx\map\flags
 ren flag_GER.bmp flag_GER_tmp.bmp
 ren flag_GER_2.bmp flag_GER.bmp
 ren flag_GER_tmp.bmp flag_GER_2.bmp
+cd ..\..\..\gfx\skins\ger
+ren topbar.bmp topbar_tmp.bmp
+ren topbar_2.bmp topbar.bmp
+ren topbar_tmp.bmp topbar_2.bmp
 
 echo German Flag is Swapped
 pause
